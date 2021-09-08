8b3a6e6e5aea1665d4ee6bd3066bce0604bd53db
Madeira <gabrielsmadeira@fac.pe.senac.br>
2021-09-08 19:14:41 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
@@ -31,7 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.authorizeRequests().anyRequest().permitAll();
 	}
-
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
